139c3ca0426e677d6308a7be4b5923e4c3330413
Will Oram <spamguy@spamguy.org>
2017-11-29 09:00:10 -0600

    Additional Firebase work.

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -2,21 +2,24 @@ import firebase from 'firebase';
 
 let messaging;
 try {
-    firebase.initializeApp({
-        apiKey: process.env.FB_API_KEY,
-        authDomain: process.env.FB_AUTH_DOMAIN,
-        databaseURL: process.env.FB_DATABASE_URL,
-        projectId: process.env.FB_PROJECT_ID,
-        storageBucket: process.env.FB_STORAGE_BUCKET,
-        messagingSenderId: process.env.FB_MESSAGING_SENDER_ID
-    });
+    // Register the service worker at a custom location.
+    navigator.serviceWorker.register('./static/firebase-messaging-sw.js')
+    .then(sw => {
+        firebase.initializeApp({
+            apiKey: process.env.FB_API_KEY,
+            authDomain: process.env.FB_AUTH_DOMAIN,
+            databaseURL: process.env.FB_DATABASE_URL,
+            // projectId: process.env.FB_PROJECT_ID,
+            storageBucket: process.env.FB_STORAGE_BUCKET,
+            messagingSenderId: process.env.FB_MESSAGING_SENDER_ID
+        });
 
-    messaging = firebase.messaging();
-    messaging.requestPermission()
-    .then(() => {
-        const fbToken = messaging.getToken();
-
-        console.log('Firebase token = ' + fbToken);
+        messaging = firebase.messaging().useServiceWorker(sw);
+        messaging.requestPermission()
+        .then(() => {
+            messaging.getToken()
+            .then(token => console.log('Firebase token = ' + token));
+        });
     });
 }
 catch (ex) {
