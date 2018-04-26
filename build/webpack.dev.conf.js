20c661d5c70685cf0f2e7575b233819bdc27327a
Will Oram <spamguy@spamguy.org>
2018-04-26 14:04:20 -0500

    Update dependencies.

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -2,6 +2,7 @@ var utils = require('./utils')
 var webpack = require('webpack')
 var config = require('../config')
 var merge = require('webpack-merge')
+var { VueLoaderPlugin } = require('vue-loader')
 var baseWebpackConfig = require('./webpack.base.conf')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 var FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
@@ -18,6 +19,7 @@ module.exports = merge(baseWebpackConfig, {
   // cheap-module-eval-source-map is faster for development
   devtool: '#cheap-module-eval-source-map',
   plugins: [
+    new VueLoaderPlugin(),
     new webpack.DefinePlugin({
       'process.env': config.dev.env
     }),
